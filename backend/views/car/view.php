cf65e02c9229d945d0406714c01ef52f87c1563a
andrkyn <rulex128@rambler.ru>
2018-05-11 13:34:46 +0300

    replaced patch to image folder & correction method

--- backend/views/car/view.php
+++ backend/views/car/view.php
@@ -53,7 +53,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 'format' => 'html',
                 //'value' =>Html::img("@web/images/{$model->img}", ['alt' => $model->name]),
                 'value'=> function($data){
-                    return Html::img('@web/images/' . $data['img']);
+                    //return Html::img('@web/images/' . $data['img']);
+                    return Html::img('/backend/web/images/' . $data->img);
 
                     //function($data) {
                     //return Html::img(Yii::getAlias('@web').'/images/'. $data['img'], ['width' => '70px']);
