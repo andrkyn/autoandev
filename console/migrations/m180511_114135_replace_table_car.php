d995d2e4606bb8a24c2fbd51325a22ab4ae49663
andrkyn <rulex128@rambler.ru>
2018-06-15 09:31:44 +0300

    delete colorId from migratio m180511_114125_replace_table_car

--- console/migrations/m180511_114135_replace_table_car.php
+++ console/migrations/m180511_114135_replace_table_car.php
@@ -10,13 +10,11 @@ class m180511_114135_replace_table_car extends Migration
 
     public function Up()
     {
-        $this->alterColumn('car','brandId', $this->integer()->notNull());
         $this->alterColumn('car','name', $this->string(255));
     }
 
     public function Down()
     {
-        $this->alterColumn('car','brandId', $this->integer());
         $this->alterColumn('car','name', $this->string(30));
     }
 
