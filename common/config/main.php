bf2fd1ec4ea3420560048d019e252a916e24de57
andrkyn <rulex128@rambler.ru>
2018-06-18 17:46:30 +0300

    added config to 'main.php'

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,9 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'image' => [
+            'class' => 'yii\image\ImageDriver',
+            'driver' => 'GD', // Imagick or GD
+        ]
     ],
 ];
